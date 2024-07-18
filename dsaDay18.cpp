dd030837059053fb63bab3fa38efb3492059a9e2
imihirjain <jmanav251@gmail.com>
2024-07-18 23:49:36 +0530

    third commit

--- dsaDay18.cpp
+++ dsaDay18.cpp
@@ -118,6 +118,49 @@ vector<vector<int>> threeSum(vector<int> &arr, int n)
     return ans;
 }
 
+/*
+    4 Sum | Find Quads that add up to a target value
+    Input Format:
+ arr[] = [1,0,-1,0,-2,2], target = 0
+Result:
+ [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Explanation:
+ We have to find unique quadruplets from the array such that the sum of those elements is equal to the target sum given that is 0. The result obtained is such that the sum of the quadruplets yields 0.
+*/
+
+vector<vector<int>> quadruplets(vector<int> &arr, int n, int target)
+{
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for(int i=0;i<n;i++)
+    {
+        if(i>0 && arr[i]==arr[i-1]) continue;
+        for(int j=i+1;j<n;j++)
+        {
+            if(j!=i+1 && arr[j]==arr[j-1])continue;
+            int k = j+1;
+            int l=n-1;
+            while(k<l)
+            {
+                long long sum = arr[i];
+                sum+=arr[j];
+                sum+=arr[k];
+                sum+=arr[l];
+                if(sum==target){
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while(k<l && arr[k]==arr[k-1]) k++;
+                    while(k<l && arr[l]==arr[l+1])l--;
+                }
+                else if(sum>target)l--;
+                else k++;
+            }
+        }
+    }
+    return ans;
+}
 int main()
 {
 
